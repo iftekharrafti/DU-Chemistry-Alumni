1230f6660bfc9c2f603d24afae62ea03c72fc088
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2023-11-30 12:54:58 +0600

    fixed title in api page

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,6 +6,8 @@ export const BASE_URL_ROOT = "https://amaderthikana.com/api";
 
 export const BASE_URL_PAYMENT = "https://amaderthikana.com/epay"
 
+export const TITLE = "DHAKA UNIVERSITY CHEMISTRY ALUMNI ASSOCIATION";
+
 export const fetchDataFromApi = async (url, params) => {
   try {
     const { data } = await axios.get(BASE_URL + url, {
