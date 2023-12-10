a769e16545890a1ce33f358060f2cc3a6a6f3277
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2023-12-10 23:33:54 +0600

    Modify president secretery

--- src/components/home/presidentSecretery/PresidentSecretery.jsx
+++ src/components/home/presidentSecretery/PresidentSecretery.jsx
@@ -12,29 +12,36 @@ const PresidentSecretary = ({ data, loading }) => {
       <Container>
         {data?.welcome?.map((item) => {
           return (
-            <Row className="mb-4" key={item.id}>
-              <Col lg={4} md={12} sm={12} className='d-flex align-items-center justify-content-center' data-aos="fade-right">
+            <Row className="mb-5" key={item.id}>
+              <Col
+                lg={4}
+                md={12}
+                sm={12}
+                className="d-flex align-items-center justify-content-center"
+                data-aos="fade-right"
+              >
                 <div className={Style.imgContent}>
-                <img
-                  src={
-                    baseImgUrl +
-                    item?.image
-                  }
-                  className={Style.img}
-                />
+                  <img src={baseImgUrl + item?.image} className={Style.img} />
+                  <div className="mt-3 text-center" style={{width: '250px'}}>
+                    <h4 className={Style.name}>{item?.name}</h4>
+                    <h5 className={Style.workplace}>{item?.workplace}</h5>
+                  </div>
                 </div>
               </Col>
               <Col lg={8} md={12} sm={12} data-aos="fade-left">
-                <div Style={Style.content}>
+                <div className={Style.content}>
                   <h2 className={Style.title}>{item?.title}</h2>
-                  <p className={Style.text1}>{item?.text1}</p>
-                  <div className="ps-4" style={{borderLeft: "2px solid #0D6EFD"}}>
-
-                  <h4 className={Style.name}>{item?.name}</h4>
-                  <h5 className={Style.workplace}>
-                    {item?.workplace}
-                  </h5>
+                  <p className={Style.text1}>{item?.text1.slice(0,650)}...</p>
+                  <div className="d-flex justify-content-center">
+                    <button className={Style.button}>Read more...</button>
                   </div>
+                  {/* <div
+                    className="ps-4"
+                    style={{ borderLeft: "2px solid #0D6EFD" }}
+                  >
+                    <h4 className={Style.name}>{item?.name}</h4>
+                    <h5 className={Style.workplace}>{item?.workplace}</h5>
+                  </div> */}
                 </div>
               </Col>
             </Row>
--- src/components/loadingSpinner/LoadingSpinner.jsx
+++ src/components/loadingSpinner/LoadingSpinner.jsx
@@ -5,7 +5,6 @@ import Style from './loadingSpinner.module.css'
 const LoadingSpinner = () => {
     return (
         <div className={Style.loadingContainer}>
-            {/* <img src="./loading.gif" alt="" className={Style.loadingGif} /> */}
             <div class={Style.ldsRoller}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
           </div>
     );
